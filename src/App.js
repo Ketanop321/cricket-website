50eba28c1901e5f985f53c058ae01414d88ba30a
Ketanop321 <146806880+Ketanop321@users.noreply.github.com>
2024-09-16 01:01:22 +0530

    Revert "adding pre loader animation"

--- src/App.js
+++ src/App.js
@@ -4,29 +4,17 @@ import Navbar from './navigation';
 import HomePage from './homepage';
 import TournamentPage from './tournament';
 import Footer from './footer';
+
+import ExploreTournaments from './exploretournaments';
 import ContactUs from './contactus';
 import LearnMorePage from './LearnMorePage';
 import RegisterPage from './RegisterPage';
 import LiveScore from './livescore';
 import PlayerPage from './player';
 import TeamPage from './teampage';
-import Preloader from './components/preloader'; // Import the preloader component
 import TournamentDetailPage from './TournamentDetailPage'; // New page for tournament details
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a data fetching process
-    setTimeout(() => {
-      setLoading(false); // Set loading to false after 3 seconds
-    }, 3000);
-  }, []);
-
-  if (loading) {
-    return <Preloader />; // Show the preloader while loading is true
-  }
-
   return (
     <Router>
       <div>
@@ -38,9 +26,14 @@ function App() {
           <Route path="/tournaments/:id" element={<TournamentDetailPage />} /> {/* Dynamic Route */}
           <Route path="/learn-more" element={<LearnMorePage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path='/exploretournaments' element={<ExploreTournaments />} />
+          <Route path='/contact' element={<ContactUs />} />
+          <Route path='/players' element={<PlayerPage />} />
+          <Route path='/teams' element={<TeamPage />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/players" element={<PlayerPage />} />
           <Route path="/teams" element={<TeamPage />} />
+
         </Routes>
         <Footer />
       </div>
